5f537d0109d97808e1f31c5872429d0a9dff42ed
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-02-07 11:58:25 +0530

    -- solved times --

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,17 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
+function sum(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
 function calculateTime(n) {
-    return 0.01;
+  const beforeTime = Date.now();
+  sum(n);
+  const afterTime = Date.now();
+  return (afterTime - beforeTime) / 1000;
 }
