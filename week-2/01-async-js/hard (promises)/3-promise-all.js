b939b0036f750acc20cdf3dc758d8c86434ee786
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-02-08 17:19:32 +0530

    solved-3-promise.all

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,35 @@
  */
 
 function wait1(t) {
-
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(t);
+    }, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(t);
+    }, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(t);
+    }, t * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+  let beforeTime = Date.now();
+  return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then((values) => {
+    let afterTime = Date.now();
+    return afterTime - beforeTime;
+  });
 }
 
 module.exports = calculateTime;
