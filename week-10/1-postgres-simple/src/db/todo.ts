c67642ea3a03064824d2fa2a0776a848951e252a
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-02-28 13:21:16 +0530

    assignment-done-week-10/1-postgres-simple

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -9,9 +9,21 @@ import { client } from "..";
  *  id: number
  * }
  */
-export async function createTodo(userId: number, title: string, description: string) {
-    
+export async function createTodo(
+  userId: number,
+  title: string,
+  description: string
+) {
+  try {
+    const query = `INSERT INTO todos (user_id, title, description) VALUES ($1, $2, $3) RETURNING * `;
+    const values = [userId, title, description];
+    const insetQuery = await client.query(query, values);
+    return insetQuery.rows[0];
+  } catch (err) {
+    console.error("Error during the insertion:", err);
+  }
 }
+
 /*
  * mark done as true for this specific todo.
  * Should return a todo object
@@ -23,9 +35,12 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+  const query = `UPDATE todos
+    SET done = true
+    WHERE id = $1 RETURNING *`;
+  const updateQuery = await client.query(query, [todoId]);
+  return updateQuery.rows[0];
 }
-
 /*
  *  Get all the todos of a given user
  * Should return an array of todos
@@ -37,5 +52,13 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+  try {
+    const getQuery = `SELECT * from todos WHERE user_id = $1 `;
+    const id = [userId];
+    const result = await client.query(getQuery, id);
+    return result.rows;
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 }
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -9,8 +9,19 @@ import { client } from "..";
  *   name: string
  * }
  */
-export async function createUser(username: string, password: string, name: string) {
-    
+export async function createUser(
+  username: string,
+  password: string,
+  name: string
+) {
+  try {
+    const query = `INSERT INTO users (username,password,name) VALUES ($1, $2, $3) RETURNING * `;
+    const values = [username, password, name];
+    const insetQuery = await client.query(query, values);
+    return insetQuery.rows[0]
+  } catch (err) {
+    console.error("Error during the insertion:", err);
+  }
 }
 
 /*
@@ -22,5 +33,12 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+  try {
+    const query = `SELECT * FROM users WHERE id = $1` ;
+    const values = [userId];
+    const getQuery = await client.query(query, values);
+    return getQuery.rows[0]
+  } catch (err) {
+    console.error("Error during fetching:", err);
+  }
 }
